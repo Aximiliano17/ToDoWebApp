165ff70d3ba566a594d175c8d2e25aee743125d9
Fernando Samano <SamanoF21@gmail.com>
2020-06-12 16:26:39 -0700

    Implemented a fragment for header.

--- ToDo/src/main/java/com/todo/controllers/DashController.java
+++ ToDo/src/main/java/com/todo/controllers/DashController.java
@@ -1,8 +1,11 @@
 package com.todo.controllers;
 
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
+
+import com.todo.domain.User;
 
 
 @Controller
@@ -11,5 +14,9 @@ public class DashController {
 	@GetMapping("/dash")
 	public String getDash() {
 		return "dash.html";
+	}
+	@PostMapping("/dash")
+	public String createTask(@AuthenticationPrincipal User user) {
+		return "redirect:/tasks";
 	}
 }
