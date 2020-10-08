d6a86695f2f14458abdd7bd37a250b69eb288be4
Fernando <SamanoF21@gmail.com>
2020-10-07 22:32:43 -0700

    Fix Eclipse to GitHub connection -See Below
    After I reformatted Laptop a while ago and had to reinstall all
    programs. I must have made a connection mistake. I didn't realize push
    wasn't going through, because I could see the commit message when I
    opened GitHub. I didn't lose any code, just over a month of green
    contributions under my account.

--- ToDo/src/main/java/com/todo/repository/ProjectRepository.java
+++ ToDo/src/main/java/com/todo/repository/ProjectRepository.java
@@ -4,10 +4,12 @@ import java.util.List;
 
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Sort;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 import com.todo.domain.Project;
+import com.todo.domain.Project.Progress;
 import com.todo.domain.User;
 
 @Repository
@@ -18,4 +20,6 @@ public interface ProjectRepository extends JpaRepository<Project, Integer> {
 	Page<Project> findByUserAndProgressAndTrashFalseAndNameContains(User user,Project.Progress progress,String keyword, Pageable pageable);
 
 	List<Project> findByUserAndTrashTrue(User user);
+
+	List<Project> findByUserAndProgressAndTrashFalse(User user, Progress progress, Sort sort);
 }
