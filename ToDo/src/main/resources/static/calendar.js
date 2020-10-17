39e986a39975094aef0dea305cb873ffc059dee4
Fernando <SamanoF21@gmail.com>
2020-10-17 15:35:31 -0700

    Display a list of Tasks and Projects due on date selected

--- ToDo/src/main/resources/static/calendar.js
+++ ToDo/src/main/resources/static/calendar.js
@@ -1,23 +1,34 @@
 let today = new Date();
 let currentMonth = today.getMonth();
 let currentYear = today.getFullYear();
-let displayedDate = today;
-console.log(currentMonth);
+let displayedDate = today;//The date that has been clicked on. By default is current day.
 
 let months = ["Jan", "Feb", "Mar", "Apr", "May", "Jun", "Jul", "Aug", "Sep", "Oct", "Nov", "Dec"];
 
 let selectedTd;
 
 let monthAndYear = document.getElementById("monthAndYear");
+
 showCalendar(currentMonth, currentYear);
 
+
+function next() {
+	currentYear = (currentMonth === 11) ? currentYear + 1 : currentYear;
+	currentMonth = (currentMonth + 1) % 12;
+	showCalendar(currentMonth, currentYear);
+}
+
+function previous() {
+	currentYear = (currentMonth === 0) ? currentYear - 1 : currentYear;
+	currentMonth = (currentMonth === 0) ? 11 : currentMonth - 1;
+	showCalendar(currentMonth, currentYear);
+}
 
 //create and display Calendar
 function showCalendar(month, year) {
 
 	let firstDay = (new Date(year, month)).getDay();
 	let daysInMonth = 32 - new Date(year, month, 32).getDate();
-	let trueMonth=month+1;//Because months start with 0.
 
 	let tbl = document.getElementById("calendar-body"); // body of the calendar
 
@@ -48,23 +59,26 @@ function showCalendar(month, year) {
 			}
 
 			else {
+				let trueMonth = month + 1;//Because months start at 0, i need to +1 to compare.
+				let dateString = year + "-" + trueMonth.toString().padStart(2, 0) + "-" + date.toString().padStart(2, 0);
 				let cell = document.createElement("td");
 				let cellText = document.createTextNode(date);
+
 				if (date === today.getDate() && year === today.getFullYear() && month === today.getMonth()) {
-					cell.classList.add("bg-info");
+					cell.classList.add("bg-primary");
+					showDisplay(dateString);
 				} // color today's date
-				
+
 				//Check for dueDates matching with calendar dates and color them.
-				let dayString = year + "-" + trueMonth.toString().padStart(2, 0)  + "-" + date.toString().padStart(2,0);
 				for (let i = 0; i < projects.length; i++) {
-					if (dayString == projects[i].dueDate)
+					if (dateString == projects[i].dueDate)
 						cell.classList.add("bg-danger");
 				}
 				for (let j = 0; j < tasks.length; j++) {
-					if (dayString == tasks[j].dueDate && !cell.classList.contains('bg-danger'))
-							cell.classList.add("bg-warning");
+					if (dateString == tasks[j].dueDate && !cell.classList.contains('bg-danger'))
+						cell.classList.add("bg-warning");
 				}
-				
+
 				cell.appendChild(cellText);
 				row.appendChild(cell);
 				date++;
@@ -73,53 +87,43 @@ function showCalendar(month, year) {
 
 		tbl.appendChild(row); // appending each row into calendar body.
 	}
-
-	var table = document.getElementById("calendar");
+	createMouseEvent();//Allows me to be able to click on a cell
+}
+function createMouseEvent() {
+	let table = document.getElementById("calendar");
 	if (table != null) {
-		for (var i = 0; i < table.rows.length; i++) {
-			for (var j = 0; j < table.rows[i].cells.length; j++)
+		for (let i = 0; i < table.rows.length; i++) {
+			for (let j = 0; j < table.rows[i].cells.length; j++)
 				table.rows[i].cells[j].onclick = function() {
-					cellDisplay(event, currentMonth, currentYear);
+					let target = event.target; // where was the click?
+					if (target.tagName != 'TD') return; // not on TD? Then we're not interested
+					let day = parseInt($(event.target).text());
+					let trueMonth = currentMonth + 1;
+					let dateString = currentYear + "-" + trueMonth.toString().padStart(2, 0) + "-" + day.toString().padStart(2, 0);
+					showDisplay(dateString);//update the display when clicking a Date
 				};
 		}
 	}
-	function cellDisplay(event, month, year) {
-		let target = event.target; // where was the click?
+}
+//Updates the display with the corresponding date
+function showDisplay(date) {
+	let titleDate = document.getElementById("dayMonthAndYear");//header of calendar
+	let display = document.getElementById("display"); // body of the calendar
 
-		if (target.tagName != 'TD') return; // not on TD? Then we're not interested
-		var day = parseInt($(event.target).text());
-		highlight(target);
-		console.log(day, month, year);
-	}
+	// clearing previous display
+	titleDate.innerHTML = "";
+	display.innerHTML = "";
 
-	function highlight(td) {
-		if (selectedTd) { // remove the existing highlight if any
-			selectedTd.classList.remove('bg-warning');
+	titleDate.innerHTML = date;
+
+	for (let i = 0; i < projects.length; i++) {
+		if (projects[i].dueDate == date) {
+			display.innerHTML = "Project " + projects[i].name + " is due.";
 		}
-		selectedTd = td;
-		selectedTd.classList.add('bg-warning'); // highlight the new td
 	}
-
-	function next() {
-		console.log("testing next")
-		currentYear = (currentMonth === 11) ? currentYear + 1 : currentYear;
-		currentMonth = (currentMonth + 1) % 12;
-		showCalendar(currentMonth, currentYear);
+	for (let i = 0; i < tasks.length; i++) {
+		if (tasks[i].dueDate == date) {
+			display.innerHTML = "Task " + tasks[i].name + " is due.";
+		}
 	}
-
-	function previous() {
-		currentYear = (currentMonth === 0) ? currentYear - 1 : currentYear;
-		currentMonth = (currentMonth === 0) ? 11 : currentMonth - 1;
-		showCalendar(currentMonth, currentYear);
-	}
-
-
-	function showDisplay(date) {
-		let display = document.getElementById("display"); // body of the calendar
-
-		// clearing previous display
-		display.innerHTML = "";
-
-	}
-
 }
